f01724d6086cc143a17772762d4e4cd4980e75e2
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2022-04-22 17:23:13 +0200

    Move WOOD unit test to separate suite

--- src/test/UnitTests/TestDebuggerBridge.ts
+++ src/test/UnitTests/TestDebuggerBridge.ts
@@ -7,8 +7,9 @@ import * as path from "path";
 import {after, before, beforeEach} from "mocha";
 import {WARDuinoDebugBridgeEmulator} from "../../DebugBridges/WARDuinoDebugBridgeEmulator";
 import {WASMCompilerBridge} from "../../CompilerBridges/WASMCompilerBridge";
+import {RunTimeTarget} from "../../DebugBridges/RunTimeTarget";
+import {WOODDebugBridgeEmulator} from "../../DebugBridges/WOODDebugBridgeEmulator";
 import ErrnoException = NodeJS.ErrnoException;
-import {InterruptTypes} from "../../DebugBridges/InterruptTypes";
 
 const runPath = process.cwd();
 const warduinoSDK = `${require('os').homedir()}/Arduino/libraries/WARDuino`;
@@ -39,146 +40,198 @@ const listener = {
 let tmpdir: string = "";
 let bridge: WARDuinoDebugBridgeEmulator;
 
-async function init() {
+async function init(target: RunTimeTarget) {
     await new Promise(resolve => {
         fs.mkdtemp(path.join(os.tmpdir(), 'warduino.'), (err: ErrnoException | null, dir: string) => {
             if (err === null) {
                 tmpdir = dir;
-                bridge = new WARDuinoDebugBridgeEmulator("",
-                    undefined,
-                    tmpdir,
-                    listener,
-                    warduinoSDK
-                );
+                switch (target) {
+                    case RunTimeTarget.wood:
+                        bridge = new WOODDebugBridgeEmulator("",
+                            undefined,
+                            tmpdir,
+                            listener,
+                            warduinoSDK
+                        );
+                        break;
+                    case RunTimeTarget.emulator:
+                    default:
+                        bridge = new WARDuinoDebugBridgeEmulator("",
+                            undefined,
+                            tmpdir,
+                            listener,
+                            warduinoSDK
+                        );
+                        break;
+                }
                 resolve(null);
+                }
             }
+        )
+            ;
         });
-    });
 
-    let compilerBridge = new WASMCompilerBridge(`${wasmDirectoryPath}/fac_ok.wast`, tmpdir);
-    await compilerBridge.compile();
-}
-
-suite("WARDuinoDebugBridgeEmulator Test Suite", () => {
-
-    before(async function () {
-        await init();
-    });
-
-    test("Test Emulator Connect", () => {
-        return bridge.connect().then(result => {
-            assert.equal(result, "127.0.0.1:8192");
-        });
-    });
-
-    test("Test Emulator Disconnect", async function () {
-        bridge.disconnect();
-        fs.rm(tmpdir, {recursive: true}, err => {
-            if (err) {
-                throw new Error('Could not delete temporary directory.');
-            }
-        });
-    });
-});
-
-function isValidJSON(text: string): boolean {
-    try {
-        JSON.parse(text);
-    } catch (e) {
-        return false;
+        let compilerBridge = new WASMCompilerBridge(`${wasmDirectoryPath}/fac_ok.wast`, tmpdir);
+        await compilerBridge.compile();
     }
-    return true;
-}
 
-function receivingDumpData(json: string, text: string): boolean {
-    return json.length > 0 || text.includes("{\"pc\":");
-}
+    suite("WARDuinoDebugBridgeEmulator Test Suite", () => {
 
-suite("Debug API Test Suite (emulated)", () => {
-    before(async function () {
-        await init();
-        await bridge.connect();
-    });
-
-    beforeEach(() => {
-        bridge.client?.removeAllListeners("data");
-    });
-
-    test("Test `pause` command", function (done) {
-        bridge.client?.on("data", (data: Buffer) => {
-            const text = data.toString();
-            console.log(text);
-            if (text.includes("PAUSE!")) {
-                done();
-            }
+        before(async function () {
+            await init(RunTimeTarget.emulator);
         });
-        bridge.pause();
-    });
 
-    test("Test `step` command", function (done) {
-        bridge.client?.on("data", (data: Buffer) => {
-            const text = data.toString();
-            console.log(text);
-            if (text.includes("STEP!")) {
-                done();
-            }
+        test("Test Emulator Connect", () => {
+            return bridge.connect().then(result => {
+                assert.equal(result, "127.0.0.1:8192");
+            });
         });
-        bridge.step();
+
+        test("Test Emulator Disconnect", async function () {
+            bridge.disconnect();
+            fs.rm(tmpdir, {recursive: true}, err => {
+                if (err) {
+                    throw new Error('Could not delete temporary directory.');
+                }
+            });
+        });
     });
 
-    test("Test `dump` command", function (done) {
-        this.timeout(3000);
-        let json = "";
+    function isValidJSON(text: string): boolean {
+        try {
+            JSON.parse(text);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
 
-        bridge.client?.on("data", (data: Buffer) => {
-            const text = data.toString();
-            console.log(text);
-            if (receivingDumpData(json, text)) {
-                let lines = text.split("\n");
-                for (let i = 0; i < lines.length; i++) {
-                    if (receivingDumpData(json, lines[i])) {
-                        json += lines[i].trimEnd();
-                        if (isValidJSON(json)) {
-                            json = "";
-                            done();
-                            break;
+    function receivingDumpData(json: string, text: string): boolean {
+        return json.length > 0 || text.includes("{\"pc\":");
+    }
+
+    suite("Debug API Test Suite (emulated)", () => {
+        before(async function () {
+            await init(RunTimeTarget.emulator);
+            await bridge.connect();
+        });
+
+        beforeEach(() => {
+            bridge.client?.removeAllListeners("data");
+        });
+
+        test("Test `pause` command", function (done) {
+            bridge.client?.on("data", (data: Buffer) => {
+                const text = data.toString();
+                console.log(text);
+                if (text.includes("PAUSE!")) {
+                    done();
+                }
+            });
+            bridge.pause();
+        });
+
+        test("Test `step` command", function (done) {
+            bridge.client?.on("data", (data: Buffer) => {
+                const text = data.toString();
+                console.log(text);
+                if (text.includes("STEP!")) {
+                    done();
+                }
+            });
+            bridge.step();
+        });
+
+        test("Test `dump` command", function (done) {
+            this.timeout(3000);
+            let json = "";
+
+            bridge.client?.on("data", (data: Buffer) => {
+                const text = data.toString();
+                console.log(text);
+                if (receivingDumpData(json, text)) {
+                    let lines = text.split("\n");
+                    for (let i = 0; i < lines.length; i++) {
+                        if (receivingDumpData(json, lines[i])) {
+                            json += lines[i].trimEnd();
+                            if (isValidJSON(json)) {
+                                json = "";
+                                done();
+                                break;
+                            }
                         }
                     }
                 }
-            }
+            });
+            bridge.refresh();
         });
-        bridge.refresh();
+
+        test("Test `run` command", function (done) {
+            bridge.client?.on("data", (data: Buffer) => {
+                const text = data.toString();
+                console.log(text);
+                if (text.includes("GO!")) {
+                    done();
+                }
+            });
+            bridge.run();
+        });
+
+        after(async function () {
+            bridge.disconnect();
+            fs.rm(tmpdir, {recursive: true}, err => {
+                if (err) {
+                    throw new Error('Could not delete temporary directory.');
+                }
+            });
+        });
     });
 
-    test("Test `run` command", function (done) {
-        bridge.client?.on("data", (data: Buffer) => {
-            const text = data.toString();
-            console.log(text);
-            if (text.includes("GO!")) {
-                done();
-            }
+    suite("WOOD Debug API Test Suite (emulated)", () => {
+        before(async function () {
+            await init(RunTimeTarget.wood);
+            await bridge.connect();
         });
-        bridge.run();
-    });
 
-    test("Test `WOODDUMP` command", function (done) {
-        bridge.client?.on("data", (data: Buffer) => {
-            const text = data.toString();
-            console.log(text);
-            if (text.includes("ack!")) {
-                done();
-            }
+        beforeEach(() => {
+            bridge.client?.removeAllListeners("data");
         });
-        let binary = "NjIwMDAwMDAyMDA0MDAwMDAwMDMwNTAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA2MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAgCjYyMDAwMDAxRUQwMTA2NTYzRUQ1QzM0OThFMDgwMDAxMDBFODAzMDAwMDAzMDAwMTAwRkZGRkZGRkZGRkZGRkZGRjA2NTYzRUQ1QzM0OTZGMDAwMDAwMDQwNDAwMDAwMDAzMDAxNzAwMDAwMDAwMDEwMDAwMDAwMDAwMDAwMDAwMDcwMDAwMDA2QTAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCAKNjIwMDAwMDFFRTA3MDA2QjAwRTQwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAgCjYyMDAwMDAwNzIwNzAwRTUwMEZGMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDEgCg=="
-        bridge.client?.write(`${InterruptTypes.interruptWOODRecvState}${binary} \n`);
-    });
 
-    after(async function () {
-        bridge.disconnect();
-        fs.rm(tmpdir, {recursive: true}, err => {
-            if (err) {
-                throw new Error('Could not delete temporary directory.');
+        test("Test `WOODDUMP` command", function (done) {
+            this.timeout(10000);
+            bridge.client?.on("data", (data: Buffer) => {
+                const text = data.toString();
+                console.log(text);
+                if (text.includes("done")) {
+                    done();
+                }
+            });
+
+            class DummyState extends WOODState {
+                constructor() {
+                    super("");
+                }
+
+                async toBinary(offset: string): Promise<string[]> {
+                    return [
+                        '62000000200400000003050000000000000000000000000600000000000000000000000000 ',
+                        '62000001ED010655CDD3E59989080001000000000003000100FFFFFFFFFFFFFFFF0655CDD3E5996F000000040400000003001700000000010000000000000000070000006A000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 ',
+                        '62000001EE07006B00E400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000…00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 ',
+                        '62000000720700E500FF00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001 ',
+                        ''
+                    ];
+                }
             }
+
+            bridge.pushSession(new DummyState());
+        });
+
+        after(async function () {
+            bridge.disconnect();
+            fs.rm(tmpdir, {recursive: true}, err => {
+                if (err) {
+                    throw new Error('Could not delete temporary directory.');
+                }
+            });
         });
     });
-});
