e853ad3aebfda3217b966d2e73ec1db88b8437c7
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2022-05-04 08:53:51 +0200

    Add dummy event view

--- src/Views/EventsProvider.ts
+++ src/Views/EventsProvider.ts
@@ -1,13 +1,38 @@
 import * as vscode from 'vscode';
-import {Event} from "../State/Event";
-import {ProviderResult, TreeItem} from "vscode";
+import {ProviderResult, TreeItem, TreeItemCollapsibleState} from 'vscode';
 
-export class EventsProvider implements vscode.TreeDataProvider<Event> {
-    getChildren(element?: Event): ProviderResult<Event[]> {
+export class EventsProvider implements vscode.TreeDataProvider<EventItem> {
+    getChildren(element?: EventItem): ProviderResult<EventItem[]> {
+        if (element === undefined) {
+            return [
+                new EventItem("button1", "", TreeItemCollapsibleState.Collapsed),
+                new EventItem("hello/world", "Hello World!", TreeItemCollapsibleState.Collapsed)];
+        } else if (element.collapsibleState !== TreeItemCollapsibleState.None) {
+            let children = [new EventItem(`topic: ${element.topic}`, "")];
+            if (element.payload.length > 0) {
+                children.push(new EventItem(`payload: ${element.payload}`, ""));
+            }
+            return children;
+        }
         return undefined;
     }
 
-    getTreeItem(element: Event): TreeItem | Thenable<TreeItem> {
-        return {};
+    getTreeItem(element: EventItem): TreeItem | Thenable<TreeItem> {
+        return element;
+    }
+}
+
+class EventItem extends vscode.TreeItem {
+    topic: string;
+    payload: string;
+
+    constructor(topic: string, payload: string, treeItemCollapsibleState: TreeItemCollapsibleState = TreeItemCollapsibleState.None) {
+        let label = topic;
+        if (treeItemCollapsibleState !== TreeItemCollapsibleState.None) {
+            label = `Event for [${topic}]`;
+        }
+        super(label, treeItemCollapsibleState);
+        this.topic = topic;
+        this.payload = payload;
     }
 }
