2cc81fa0defa1621605f8aae5138e1927a1c3b82
tolauwae <Tom.Lauwaerts@UGent.be>
2022-09-23 17:00:43 +0200

    Restructure test code
    Have all testfiles in one folder.
    Clearer folder names.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode';
 import {WASMCompilerBridge} from '../../CompilerBridges/WASMCompilerBridge';
 
+// Tests specific to the extension
 suite('Extension Test Suite', () => {
 
     vscode.window.showInformationMessage('Start all tests.');
 
-    test('TestCompileBridge', () => {
+    test('Test WASM Compiler Bridge', () => {
         let compilerBridge = new WASMCompilerBridge("", "", "");
         compilerBridge.compile();
     });
