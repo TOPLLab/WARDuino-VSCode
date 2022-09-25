9a3e7fc905acf5ec492204eb3065341f30de9eaa
tolauwae <Tom.Lauwaerts@UGent.be>
2022-09-25 22:17:14 +0100

    Add pause test

--- src/test/suite/debugger.test.ts
+++ src/test/suite/debugger.test.ts
@@ -5,17 +5,48 @@ import {ChildProcess, spawn} from "child_process";
 import {Readable} from "stream";
 import {assert, expect} from "chai";
 import {ReadlineParser} from "serialport";
+import * as net from 'net';
+import {InterruptTypes} from "../../DebugBridges/InterruptTypes";
+import {DebugInfoParser} from "../../Parsers/DebugInfoParser";
+import {RuntimeState} from "../../State/RuntimeState";
+import {EventItem} from "../../Views/EventsProvider";
 
 const interpreter = `${require('os').homedir()}/Arduino/libraries/WARDuino/build-emu/wdcli`;
 const examples = 'src/test/suite/examples/';
+
+let port: number = 8192;
 
 function isReadable(x: Readable | null): x is Readable {
     return x !== null;
 }
 
-suite('Debugger Test Suite', () => {
+function connectToDebugger(program: string): Promise<net.Socket> {
+    const address = {port: port, host: "127.0.0.1"};
+    const process = spawn(interpreter, ['--socket', (port++).toString(), '--file', program]);
 
-    let port: number = 8192;
+    return new Promise(function (resolve, reject) {
+        while (process.stdout === undefined) {
+        }
+
+        if (isReadable(process.stdout)) {
+            const reader = new ReadlineParser();
+            process.stdout.pipe(reader);
+
+            reader.on('data', (data) => {
+                if (data.includes('Listening')) {
+                    const client = new net.Socket();
+                    client.connect(address, () => {
+                        resolve(client);
+                    });
+                }
+            });
+        } else {
+            reject();
+        }
+    });
+}
+
+suite('Debugger Test Suite', () => {
 
     test('Test exitcode (0)', function (done) {
         const process = spawn(interpreter, ['--no-debug', '--file', `${examples}hello.wasm`]).on('exit', function (code) {
@@ -57,5 +88,33 @@ suite('Debugger Test Suite', () => {
                 }
             });
         }
+    });
+
+    test('Test pause', async function () {
+        this.timeout(5000);
+        const socket: net.Socket = await connectToDebugger(`${examples}blink.wasm`);
+
+        // save returned program counters
+        const counters: number[] = [];
+        socket.on("data", (data: Buffer) => {
+            data.toString().split("\n").forEach((line) => {
+                if (line.startsWith("{\"pc")) {
+                    const parsed = JSON.parse(line);
+                    counters.push(parseInt(parsed.pc));
+                }
+            });
+        });
+
+        // run test
+        socket.write(`${InterruptTypes.interruptPAUSE} \n`);
+        socket.write(`${InterruptTypes.interruptDUMP} \n`);
+        await new Promise(f => setTimeout(f, 1000));
+        socket.write(`${InterruptTypes.interruptDUMP} \n`);
+
+        await new Promise(f => setTimeout(f, 1000));
+
+        // perform checks
+        expect(counters.length).to.equal(2);
+        expect(counters[0]).to.equal(counters[1]);
     });
 });
