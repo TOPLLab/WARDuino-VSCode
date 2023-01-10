4bb7af98a3a36445f9911de61f54d830588ca3d8
tolauwae <Tom.Lauwaerts@UGent.be>
2023-01-10 15:38:45 +0100

    Refactor: move bridge code in tests to separate file

--- src/test/suite/integration.test.ts
+++
@@ -1,5 +0,0 @@
-import {Describer} from '../framework/Describer';
-import {EmulatorBridge} from './debugger.test';
-const EMULATOR: string = `${require('os').homedir()}/Arduino/libraries/WARDuino/build-emu/wdcli`;
-
-const cli: Describer = new Describer(new EmulatorBridge(EMULATOR));
