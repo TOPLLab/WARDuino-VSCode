20d2ba74e8777de45b3b3e06b1d2c5ba59c160b9
tolauwae <Tom.Lauwaerts@UGent.be>
2023-01-20 17:14:51 +0100

    Fix: add missing Parsers.ts

---
+++ src/test/framework/Parsers.ts
@@ -0,0 +1,35 @@
+import {FunctionInfo} from '../../State/FunctionInfo';
+
+export function parseExport(input: string): FunctionInfo[] {
+    const results: FunctionInfo[] = [];
+    const section: string[] = consumeUntil(input, 'Export').split('\n');
+    section.pop();
+    for (const line of section) {
+        const index: number = getIndex(line);
+        const name: string = getName(line);
+        if (0 <= index && 0 < name.length) {
+            results.push({index: index, name: name, locals: []});
+        }
+    }
+    return results;
+}
+
+function consumeUntil(text: string, until: string): string {
+    return text.split(until)[1] ?? '';
+}
+
+function getIndex(line: string): number {
+    const match = /func\[([0-9]+)\]/.exec(line);
+    if (match === null || match[1] === undefined) {
+        return -1;
+    }
+    return parseInt(match[1]);
+}
+
+function getName(line: string): string {
+    const match = /-> "([a-zA-Z0-9]+)"/.exec(line);
+    if (match === null || match[1] === undefined) {
+        return '';
+    }
+    return match[1];
+}
