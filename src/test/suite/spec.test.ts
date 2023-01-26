6a31f50603465cbd1584cbf82e8e6e1239317024
tolauwae <Tom.Lauwaerts@UGent.be>
2023-01-26 17:09:05 +0100

    Read assert files + start on parsing

--- src/test/suite/spec.test.ts
+++ src/test/suite/spec.test.ts
@@ -3,6 +3,9 @@ import {Interrupt} from '../framework/Actions';
 import {Framework} from '../framework/Framework';
 import {EMULATOR, EmulatorBridge} from './warduino.bridge';
 import {encode, returnParser} from './spec.util';
+import {createReadStream, readdirSync} from 'fs';
+import * as readline from 'readline';
+import {find} from '../framework/Parsers';
 
 const framework = Framework.getImplementation();
 
@@ -10,26 +13,35 @@ framework.platform(new EmulatorBridge(EMULATOR));
 
 framework.suite('WebAssembly Spec tests');
 
-const files: string[] = [];
+const files: string[] = readdirSync('core');
 
 for (const file of files) {
-    const module: string = '';
+    if (!file.endsWith('.assert.wast')) {
+        // only look at assert files
+        break;
+    }
 
-    // for each module
+    const module: string = file.replace('.asserts.wast', '.wast');
 
-    const asserts: string[] = [];
+    parseAsserts(file).then((asserts: string[]) => createTest(module, asserts));
+}
 
+function createTest(module: string, asserts: string[]) {
     const steps: Step[] = [];
 
     for (const assert of asserts) {
+        const fidx: string = find(/invoke "([^"]+)"/, assert);
+        const args: number[] = [0, 0];  // todo parse
+        const result: number = 0;  // todo parse
+
         steps.push({
-            // (assert_return (invoke "add" (f32.const 0x0p+0) (f32.const 0x0p+0)) (f32.const 0x0p+0))
+            // (invoke "add" (f32.const 0x0p+0) (f32.const 0x0p+0)) (f32.const 0x0p+0)
             title: assert,
             instruction: Interrupt.invoke,
-            payload: encode(module, 'add', [0, 0]),
+            payload: encode(module, fidx, args),
             parser: returnParser,
             expected: [{
-                'value': {kind: 'primitive', value: 0} as Expected<number>
+                'value': {kind: 'primitive', value: result} as Expected<number>
             }]
         });
     }
@@ -39,5 +51,20 @@ for (const file of files) {
         program: module,
         dependencies: [],
         steps: steps
+    });
+}
+
+function parseAsserts(file: string): Promise<string[]> {
+    return new Promise<string[]>((resolve) => {
+        const asserts: string[] = [];
+        const reader = readline.createInterface(createReadStream(file));
+
+        reader.on('line', (line) => {
+            asserts.push(line.replace('(assert_return', '('));
+        });
+
+        reader.on('close', () => {
+            resolve(asserts);
+        });
     });
 }
