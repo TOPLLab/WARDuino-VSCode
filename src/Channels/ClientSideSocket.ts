782066e6ab0fbe90fd12de5d5b5986cd203b762c
Carlos Rojas <carlosj.rojas.castillo@gmail.com>
2023-01-31 20:10:01 +0100

    bug fix: buffer content until new line reached

--- src/Channels/ClientSideSocket.ts
+++ src/Channels/ClientSideSocket.ts
@@ -2,19 +2,20 @@ import * as net from 'net';
 import { EventEmitter } from 'stream';
 
 
-export class ClientSideSocket extends EventEmitter{
+export class ClientSideSocket extends EventEmitter {
 
     private port: number;
     private host: string;
     private connection: net.Socket | undefined;
+    private dataBuffered: string = "";
 
     constructor(port: number, host: string) {
         super();
         this.port = port;
         this.host = host;
     }
-    
-    write(data: string, cb?: ((err?: Error| undefined) => void) | undefined): boolean {
+
+    write(data: string, cb?: ((err?: Error | undefined) => void) | undefined): boolean {
         return !!this.connection && this.connection.write(data);
     }
 
@@ -23,11 +24,19 @@ export class ClientSideSocket extends EventEmitter{
             const addr = { port: this.port, host: this.host };
             let con = new net.Socket();
             let attemptedConnections = 1;
-            con.connect(addr, () => {console.log(`ClientSideSocket: connecting to ${this.host}:${this.port}`);});
+            con.connect(addr, () => { console.log(`ClientSideSocket: connecting to ${this.host}:${this.port}`); });
 
-            con.on('data', (data: Buffer) =>{
-                //TODO emit data just for each newline
-                this.emit('data', data.toString('ascii'));
+            con.on('data', (data: Buffer) => {
+                this.dataBuffered += data.toString('ascii');
+                const splits = this.dataBuffered.split('\n');
+                this.dataBuffered = splits[splits.length - 1];
+                for (let index = 0; index < splits.length - 1; index++) {
+                    const content = splits[index];
+                    if (content === '') {
+                        continue;
+                    }
+                    this.emit('data', content);
+                }
             });
 
             con.on('connect', () => {
@@ -39,7 +48,7 @@ export class ClientSideSocket extends EventEmitter{
                 if (!!!this.connection && attemptedConnections >= maxAttempts) {
                     resolve(false);
                 }
-                if(!!!this.connection){
+                if (!!!this.connection) {
                     attemptedConnections++;
                     con.connect(addr);
                 }
