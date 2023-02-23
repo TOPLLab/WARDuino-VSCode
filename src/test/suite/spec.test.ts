c69f735dc07cf674b80ea714e89d9ee82b816751
tolauwae <Tom.Lauwaerts@UGent.be>
2023-02-23 11:41:17 +0100

    Add `TESTFILE` env variable

--- src/test/suite/spec.test.ts
+++ src/test/suite/spec.test.ts
@@ -1,41 +1,54 @@
 import {Expectation, Expected, Step} from '../framework/Describer';
 import {Instruction} from '../framework/Actions';
 import {Framework} from '../framework/Framework';
-import {EMULATOR, EmulatorBridge} from './warduino.bridge';
+import {ARDUINO, EMULATOR, EmulatorBridge, HardwareBridge} from './warduino.bridge';
 import {parseArguments, parseAsserts, parseResult, Value} from './spec.util';
 import {readdirSync, writeFileSync} from 'fs';
 import {find} from '../framework/Parsers';
+import {HybridScheduler} from '../framework/Scheduler';
 import {basename} from 'path';
 
 export const CORESUITE: string = process.env.CORESUITE ?? './';
 
+const TESTFILE: string = process.env.TESTFILE ?? '';
+
 const framework = Framework.getImplementation();
 
-framework.platform(new EmulatorBridge(EMULATOR));
+framework.platform(new EmulatorBridge(EMULATOR), new HybridScheduler());
+// framework.platform(new HardwareBridge(ARDUINO), new HybridScheduler());
 
 framework.suite('WebAssembly Spec tests');
 
-process.stdout.write(`> Scanning suite: ${CORESUITE}\n\n`);
-
-const files: string[] = readdirSync(CORESUITE).filter((file) => file.endsWith('.asserts.wast'));
-
-let count = 0;
-let tally: string = ` [${count++}/${files.length}]`;
-process.stdout.write(`> Building scenarios${tally}`);
-for (const file of files) {
-    const module: string = file.replace('.asserts.wast', '.wast');
-    const asserts: string[] = parseAsserts(CORESUITE + file);
+if (TESTFILE.length > 0) {
+    const module: string = TESTFILE.replace('.asserts.wast', '.wast');
+    const asserts: string[] = parseAsserts(CORESUITE + TESTFILE);
     createTest(CORESUITE + module, asserts);
+    framework.run();
+} else {
+    process.stdout.write(`> Scanning suite: ${CORESUITE}\n\n`);
 
-    if (process.stdout.moveCursor !== undefined) {
-        process.stdout.moveCursor(-tally.length, 0);
+    const files: string[] = readdirSync(CORESUITE).filter((file) => file.endsWith('.asserts.wast'));
+
+    let count = 0;
+    let tally: string = ` [${count++}/${files.length}]`;
+    process.stdout.write(`> Building scenarios${tally}`);
+    for (const file of files) {
+        const module: string = file.replace('.asserts.wast', '.wast');
+        const asserts: string[] = parseAsserts(CORESUITE + file);
+        createTest(CORESUITE + module, asserts);
+
+        if (process.stdout.moveCursor !== undefined) {
+            process.stdout.moveCursor(-tally.length, 0);
+        }
+        tally = ` [${count++}/${files.length}]`;
+        process.stdout.write(tally);
     }
-    tally = ` [${count++}/${files.length}]`;
-    process.stdout.write(tally);
-}
 
-process.stdout.write('\n\n> Starting framework (this may take a while)\n\n');
-framework.run();
+    process.stdout.write('\n\n> Starting framework (this may take a while)\n\n');
+
+    framework.run();
+
+}
 
 function createTest(module: string, asserts: string[]) {
     const steps: Step[] = [];
