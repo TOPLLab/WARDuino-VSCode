823a3b9ec68a9ae1016b545f529d645e5ecceb7a
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-05 10:25:44 +0200

    removed unneeded code

--- src/Parsers/DebugInfoParser.ts
+++ src/Parsers/DebugInfoParser.ts
@@ -1,16 +1,11 @@
 import { DebugBridge } from "../DebugBridges/DebugBridge";
-import { VariableInfo } from "../State/VariableInfo";
-import { Frame } from "./Frame";
 import { EventItem } from "../Views/EventsProvider";
 import { RuntimeState } from "../State/RuntimeState";
-import { WOODState } from "../State/WOODState";
-import { WasmState } from "../State/AllState";
 import { SourceMap } from "../State/SourceMap";
 import { BreakpointPolicy } from "../State/Breakpoint";
 
 export class DebugInfoParser {
 
-    private addressBeginning: number = 0;
     private sourceMap: SourceMap;
 
     constructor(sourceMap: SourceMap) {
@@ -30,7 +25,6 @@ export class DebugInfoParser {
             let breakpointInfo = line.match(/AT ([0-9]+)!/);
             if (breakpointInfo.length > 1) {
                 let pc = parseInt(breakpointInfo[1]);
-                bridge.setProgramCounter(pc);
                 bridge.pause();
                 if (bridge.getBreakpointPolicy() === BreakpointPolicy.singleStop) {
                     bridge.getListener().notifyInfoMessage(`Enforcing '${BreakpointPolicy.singleStop}' breakpoint policy`);
@@ -54,7 +48,7 @@ export class DebugInfoParser {
             const evts = JSON.parse(line).events;
             if (!!rs && !!evts) {
                 rs.setEvents(evts.map((obj: EventItem) => (new EventItem(obj.topic, obj.payload))));
-                bridge?.refreshEvents(rs.events);
+                bridge?.refreshViews();
             } else {
                 if (!!!rs) {
                     console.error("DebugInfoParser: no runtimestate to set events upon");
@@ -66,30 +60,8 @@ export class DebugInfoParser {
         } else if (line.startsWith("{\"pc")) {
             const runtimeState: RuntimeState = new RuntimeState(line, this.sourceMap);
             bridge.updateRuntimeState(runtimeState);
-            console.log(`PC=${bridge.getProgramCounter()} (Hexa ${bridge.getProgramCounter().toString(16)})`);
+            const currentState = bridge.getCurrentState();
+            console.log(`PC=${currentState!.getProgramCounter()} (Hexa ${currentState!.getProgramCounter().toString(16)})`);
         }
-    }
-
-    private parseLocals(fidx: number, bridge: DebugBridge, objs: any[]): VariableInfo[] {
-        let locals: VariableInfo[] = bridge.getLocals(fidx);
-        objs.forEach((obj) => {
-            let local = locals[obj.index];
-            if (local) {
-                local.type = obj.type;
-                local.value = obj.value;
-            }
-        });
-        console.log(locals);
-        return locals;
-    }
-
-    private parseCallstack(objs: any[]): Frame[] {
-        let functions: Frame[] = [];
-        objs.filter((obj) => {
-            return obj.type === 0;
-        }).forEach((obj) => {
-            functions.push({ index: parseInt(obj.fidx), returnAddress: parseInt(obj.callsite) - this.addressBeginning });
-        });
-        return functions;
     }
 }
