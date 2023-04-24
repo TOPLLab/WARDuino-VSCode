5e22edd6326ae515d84bf302a692d725c4e8bf65
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-24 15:49:28 +0200

    add request for exception on start
    When connecting the debugger cient to the MCU.
    We request any exception that might have occurred.
    This lets show to the user any potential
    exceptions that occured while the debugger client was not connected.

--- src/DebugSession/DebugSession.ts
+++ src/DebugSession/DebugSession.ts
@@ -208,6 +208,7 @@ export class WARDuinoDebugSession extends LoggingDebugSession {
                 this.onPause();
             }
             else {
+                this.debugBridge!.requestStoredException(); // no need to await
                 this.onRunning();
             }
         }
