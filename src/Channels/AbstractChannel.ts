5ed5858129a36ab293a3f3968809c9bf4b4d3de9
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-25 00:47:22 +0200

    disconnect from channel appropriately

--- src/Channels/AbstractChannel.ts
+++ src/Channels/AbstractChannel.ts
@@ -28,6 +28,8 @@ export abstract class AbstractChannel implements ChannelInterface {
 
     public abstract openConnection(maxAttempts?: number): Promise<boolean>;
 
+    public abstract disconnect(): void;
+
     public addCallback(dataCheck: (line: string) => boolean, cb: (line: string) => void): void {
         this.callbacks.push([dataCheck, cb]);
     }
@@ -42,11 +44,6 @@ export abstract class AbstractChannel implements ChannelInterface {
             this.requests.push([request, res]);
             this.write(request.dataToSend, rej);
         });
-    }
-
-    public disconnect(): void {
-        this.connection?.close();
-        this.connection = undefined;
     }
 
     private catchAllLogger(line: string) {
--- src/Channels/SerialConnection.ts
+++ src/Channels/SerialConnection.ts
@@ -1,39 +1,43 @@
-import {ReadlineParser, SerialPort} from 'serialport';
+import { ReadlineParser, SerialPort } from 'serialport';
 
-export class LoggingSerialMonitor{
+export class LoggingSerialMonitor {
 
     public readonly loggername: string;
     public readonly baudrate: number;
     public readonly port: string;
     private connection: any;
 
-    constructor(loggername: string, port: string, baudrate: number ){
+    constructor(loggername: string, port: string, baudrate: number) {
         this.loggername = `${loggername} (Monitor)`;
         this.baudrate = baudrate;
         this.port = port;
     }
 
     openConnection(): Promise<void> {
-        return new Promise<void>((resolve, reject) =>{
-            let con = new SerialPort({path: this.port, baudRate: this.baudrate},
-            (error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    this.connection = con;
-                    this.installLogger();
-                    resolve();
-                }
-            });
+        return new Promise<void>((resolve, reject) => {
+            let con = new SerialPort({ path: this.port, baudRate: this.baudrate },
+                (error) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        this.connection = con;
+                        this.installLogger();
+                        resolve();
+                    }
+                });
         });
 
     }
 
-    private installLogger(): void{
+    disconnect(): void {
+        this.connection?.close();
+    }
+
+    private installLogger(): void {
         const parser = new ReadlineParser();
         this.connection?.pipe(parser);
         parser.on("data", (line: string) => {
             console.log(`${this.loggername}: ${line}`);
         });
     }
- }
+}
