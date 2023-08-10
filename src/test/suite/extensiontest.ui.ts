0d98e5389676441c4eb064a2c06f5e51ef058263
Carlos Rojas <carlosj.rojascastillo@gmail.com>
2023-08-10 19:41:38 +0200

    remove redundand test

--- src/test/suite/extensiontest.ui.ts
+++ src/test/suite/extensiontest.ui.ts
@@ -1,13 +1,3 @@
-import * as vscode from 'vscode';
-import {WASMCompilerBridge} from '../../CompilerBridges/WASMCompilerBridge';
-
 // Tests specific to the extension
 describe('Extension Test Suite', () => {
-
-    vscode.window.showInformationMessage('Start all tests.');
-
-    it('Test WASM Compiler Bridge', () => {
-        let compilerBridge = new WASMCompilerBridge('', '', '');
-        compilerBridge.compile();
-    });
 });
